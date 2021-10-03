89dccd700ad825b65364e2224c6d57e2c449f1c0
Tung Bui <tungbq.info@gmail.com>
2021-10-03 17:00:42 +0700

    Correct files

--- leetcode/14days-algo/5-Remove-N-Element-Linked-List.js
+++ leetcode/14days-algo/5-Remove-N-Element-Linked-List.js
@@ -15,26 +15,35 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function (head, n) {
+ var removeNthFromEnd = function (head, n) {
 	// Calculate length of list
 	let cur_node = head;
 	let length = 1;
 	while (cur_node.next !== null) {
+		cur_node = cur_node.next;
 		length += 1;
 	}
 
-	// Find index of element to remove
+	// Find index of the previous element of element to be removed
 	const indexToRemove = length - n - 1;
 
-	// Iterate to update linkedlist
-	cur_node = head;
-	for (let i = 0; i < length; i++) {
-		if (i === indexToRemove) {
-			// Remove next element
-			cur_node.next = cur_node.next.next;
+	// Cover case remove the 'head'
+	if (indexToRemove === -1) {
+		head = head.next;
+	} else {
+		// Iterate to update linkedlist
+		cur_node = head;
+		let i = 0;
+		while (cur_node !== null && cur_node.next !== null) {
+			if (i === indexToRemove) {
+				// Remove element
+				cur_node.next = cur_node.next.next;
+			}
+			cur_node = cur_node.next;
+			i++;
 		}
-		cur_node = cur_node.next;
 	}
+	return head;
 };
 
 // TODO: Do this in one pass...
