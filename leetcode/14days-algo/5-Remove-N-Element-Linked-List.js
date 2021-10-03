d46de84a2e91f7191eb4573fbb963c3aa2e3c2fb
Tung Bui <tungbq.info@gmail.com>
2021-10-03 16:25:00 +0700

    Add Remove Nth Node From End of List

---
+++ leetcode/14days-algo/5-Remove-N-Element-Linked-List.js
@@ -0,0 +1,23 @@
+// Remove Nth Node From End of List
+
+// Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  // TODO    
+};
+
+// Input: 
+head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
