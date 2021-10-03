39630d10242ad8b0ce1ce3152614571a02ac3e5a
Tung Bui <tungbq.info@gmail.com>
2021-10-03 11:12:11 +0700

    Add reverse-words-in-a-string-iii

---
+++ leetcode/14days-algo/4-Reverse_Words_In_A_String_III.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+	let output = '';
+  const s_arr = s.split(' ');
+	for (var word of s_arr) {
+		const isFirstElement = s_arr.indexOf(word) === 0;
+		word = word.split('');
+		let left = 0;
+		let right = word.length - 1;
+
+		while (left < right) {
+			let tmp = word[left];
+			word[left] = word[right];
+			word[right] = tmp;
+			left += 1;
+			right -= 1;
+		}
+
+		if (isFirstElement) {
+			output = word.join('');
+		} else {
+			output += ` ${word.join('')}`;
+		}
+	}
+
+	return output;
+};
+
+s = "Let's take LeetCode contest";
+console.log(reverseWords(s));
+// Output: "s'teL ekat edoCteeL tsetnoc"
