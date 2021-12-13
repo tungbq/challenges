cb6778125b0ae2dfba6f81ee67e16e71aa061b58
Tung Bui <tungbq.info@gmail.com>
2021-12-13 23:09:21 +0700

    Add pacehoders

---
+++ leetcode/14days-algo/12-Climbing_Stairs.js
@@ -0,0 +1,13 @@
+// You are climbing a staircase. It takes n steps to reach the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    
+};
+
+n = 3
+// Output: 3
