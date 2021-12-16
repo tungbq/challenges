d58b5c5e167fbc73ab198f30d4a4c4b90419bec3
Tung Bui <tungbq.info@gmail.com>
2021-12-16 21:44:07 +0700

    [12] Add climbing stairs

--- leetcode/14days-algo/12-Climbing_Stairs.js
+++ leetcode/14days-algo/12-Climbing_Stairs.js
@@ -5,9 +5,15 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
-    
+var climbStairs = function (n, memo = {}) {
+	if (n in memo) return memo[n];
+	if (n === 0) return 1;
+	if (n < 0) return 0;
+
+	memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+	return memo[n];
 };
 
-n = 3
+n = 3;
 // Output: 3
+console.log(climbStairs(n));
