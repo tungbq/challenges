eb181dc988a20092b19c532b5dbf1f5334a0d861
Tung Bui <tungbq.info@gmail.com>
2021-12-26 22:51:18 +0700

    [14] reverse bits

---
+++ leetcode/14days-algo/14-Reverse_Bits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+ var reverseBits = function(n) {
+  var result = 0;
+  var count = 32;
+
+  while (count--) {
+    result *= 2;
+    result += n & 1;
+    n = n >> 1;
+  }
+  return result;
+};
+
