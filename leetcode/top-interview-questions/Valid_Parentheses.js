74342f3e12fbd8723826c0b70400c400c71c9525
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2022-02-13 09:38:19 +0700

    [top-interview] Solution for valid parentthese

--- leetcode/top-interview-questions/Valid_Parentheses.js
+++ leetcode/top-interview-questions/Valid_Parentheses.js
@@ -3,13 +3,29 @@
  * @return {boolean}
  */
 var isValid = function (s) {
+	const open_brackets = ['{', '[', '('];
+	const close_brackets = ['}', ']', ')'];
+	let track_open_elements = [];
+
 	// loop through each char
-	for (let i = 0; i < s.length; i += 2) {
-		if (
-			s[i] + s[i + 1] !== '[]' &&
-			s[i] + s[i + 1] !== '()' &&
-			s[i] + s[i + 1] !== '{}'
-		) {
+	for (let i = 0; i < s.length; i++) {
+		if (open_brackets.includes(s[i])) {
+			// in case string ends of with open brackets, treat it as 'false'
+			if (i === s.length - 1) {
+				return false;
+			}
+
+			track_open_elements.push(s[i]);
+		} else {
+			// case close bracket
+			if (
+				close_brackets.indexOf(s[i]) !==
+				open_brackets.indexOf(track_open_elements.pop())
+			) {
+				return false;
+			}
+		}
+		if (i === s.length - 1 && track_open_elements.length > 0) {
 			return false;
 		}
 	}
@@ -19,15 +35,35 @@ var isValid = function (s) {
 
 // Test case 1:
 // Input:
-s = '()[]{}{';
+s = '()[]{}';
 // Output: true
 console.log('-----Output----');
 console.log(isValid(s));
 
 // Test case 2:
-// TODO: Cover this case
 // Input:
-s = '{[]}';
+s = '{[()]}';
 // Output: true
+console.log('-----Output----');
+console.log(isValid(s));
+
+// Test case 3:
+// Input:
+s = '[';
+// Output: false
+console.log('-----Output----');
+console.log(isValid(s));
+
+// Test case 4:
+// Input:
+s = '"([]"';
+// Output: false
+console.log('-----Output----');
+console.log(isValid(s));
+
+// Test case 5:
+// Input:
+s = '[])';
+// Output: false
 console.log('-----Output----');
 console.log(isValid(s));
