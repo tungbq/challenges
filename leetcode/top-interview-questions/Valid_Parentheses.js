b97ef6d33956e73469bad96aa9cf460d619b255b
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2022-02-13 08:53:54 +0700

    [top-interview] add Valid Parentness (WIP)

---
+++ leetcode/top-interview-questions/Valid_Parentheses.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+	// loop through each char
+	for (let i = 0; i < s.length; i += 2) {
+		if (
+			s[i] + s[i + 1] !== '[]' &&
+			s[i] + s[i + 1] !== '()' &&
+			s[i] + s[i + 1] !== '{}'
+		) {
+			return false;
+		}
+	}
+	// check each pair
+	return true;
+};
+
+// Test case 1:
+// Input:
+s = '()[]{}{';
+// Output: true
+console.log('-----Output----');
+console.log(isValid(s));
+
+// Test case 2:
+// Input:
+s = '{[]}';
+// Output: true
+console.log('-----Output----');
+console.log(isValid(s));
