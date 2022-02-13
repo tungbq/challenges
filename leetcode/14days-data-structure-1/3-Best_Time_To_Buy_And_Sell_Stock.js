9f19b91400e1f739ff50cc724f107b75ced7b04a
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2022-02-13 08:43:34 +0700

    place holder DS + init top interview questions folder

---
+++ leetcode/14days-data-structure-1/3-Best_Time_To_Buy_And_Sell_Stock.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {};
+
+prices = [7, 1, 5, 3, 6, 4];
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+console.log(maxProfit(prices));
