e43283864e6ed94a7f7b9fe924e867e89c7093d7
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2022-07-02 18:43:09 +0700

    [14-DS-2022] [PlaceHolder] 6-RansomNote.js

---
+++ leetcode/14-days-ds-2022/6-RansomNote.js
@@ -0,0 +1,26 @@
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed
+//  by using the letters from magazine and false otherwise.
+// Each letter in magazine can only be used once in ransomNote.
+
+// Example 1:
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+
+// Example 2:
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+
+  
+};
+
+(ransomNote = 'aa'), (magazine = 'aab');
+// Output: true
+
+console.log(canConstruct(ransomNote, magazine));
