d1b2f158919487b58dc5acb68735a5f6c0cce0f7
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2023-08-31 20:55:11 +0700

    [Leetcode75] find diff of two arrays - using set

--- leetcode/leetcode75/find_the_difference_of_two_arrays.py
+++ leetcode/leetcode75/find_the_difference_of_two_arrays.py
@@ -8,8 +8,12 @@ Note that the integers in the lists may be returned in any order.
 
 class Solution:
     def findDifference(self, nums1, nums2):
-        nums1_res = self.baseFindDifference(nums1, nums2)
-        nums2_res = self.baseFindDifference(nums2, nums1)
+        # Reducing the array size by converting it to set
+        nums1_set = set(nums1)
+        nums2_set = set(nums2)
+
+        nums1_res = self.baseFindDifference(nums1_set, nums2_set)
+        nums2_res = self.baseFindDifference(nums2_set, nums1_set)
         return [nums1_res, nums2_res]
 
     def baseFindDifference(self, nums1, nums2):
