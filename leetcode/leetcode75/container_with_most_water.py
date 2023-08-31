76cb94ee9d87e7fe27c82f2591566e44d9fdda15
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2023-08-31 19:37:41 +0700

    [Leetcode75] Init container with most water - example

--- leetcode/leetcode75/container_with_most_water.py
+++ leetcode/leetcode75/container_with_most_water.py
@@ -11,7 +11,10 @@ Notice that you may not slant the container.
 """
 
 """
-
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
+In this case, the max area of water (blue section) the container can contain is 49
 """
 
 class Solution:
