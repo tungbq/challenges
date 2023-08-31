718e997fe45afe797999910ecdbff70f78abe21a
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2023-08-31 19:36:35 +0700

    [Leetcode75] Init container with most water

---
+++ leetcode/leetcode75/test/container_with_most_water_test.py
@@ -0,0 +1,19 @@
+import unittest
+import sys
+sys.path.append("..")
+from container_with_most_water import Solution
+
+class TestSolution(unittest.TestCase):
+
+    def test_case_1(self):
+        sol = Solution()
+        result = sol.canPlaceFlowers([1,8,6,2,5,4,8,3,7])
+        self.assertEqual(result, 49)
+
+    def test_case_2(self):
+        sol = Solution()
+        result = sol.canPlaceFlowers([1,1])
+        self.assertEqual(result,1)
+
+if __name__ == '__main__':
+    unittest.main()
