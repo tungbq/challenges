318cceff808bca43cf080580887e9f9c64b75da0
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2023-08-31 18:38:19 +0700

    [Leetcode75] Init find pivot index

---
+++ leetcode/leetcode75/test/find_pivot_index_test.py
@@ -0,0 +1,40 @@
+import unittest
+import sys
+sys.path.append("..")
+from find_pivot_index import Solution
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case_1(self):
+        sol = Solution()
+        result = sol.isSubsequence("abc", "ahbgdc")
+        self.assertEqual(result, True)
+
+    def test_case_2(self):
+        sol = Solution()
+        result = sol.isSubsequence("axc", "ahbgdc")
+        self.assertEqual(result, False)
+
+    def test_case_3(self):
+        sol = Solution()
+        result = sol.isSubsequence("aa", "aa")
+        self.assertEqual(result, True)
+
+    def test_case_4(self):
+        sol = Solution()
+        result = sol.isSubsequence("a", "b")
+        self.assertEqual(result, False)
+
+    def test_case_5(self):
+        sol = Solution()
+        result = sol.isSubsequence("cvbn", "qwryufhhcsffvqfrboiopjehn")
+        self.assertEqual(result, True)
+
+    def test_case_6(self):
+        sol = Solution()
+        result = sol.isSubsequence("fqwfhwqiufjwoiqfiwhfihwf", "qwryufhhcsffvqfrbwdfoiopjehn")
+        self.assertEqual(result, False)
+
+if __name__ == '__main__':
+    unittest.main()
