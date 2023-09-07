ba86a920681e2a7f237f6be0e3128b037973befc
Tung Bui <tung.bquang@gmail.com>
2023-09-07 23:50:40 +0700

    [Leetcode] maximum_average_subarray_i - solution

---
+++ leetcode/leetcode75/test/maximum_average_subarray_i_test.py
@@ -0,0 +1,19 @@
+import unittest
+import sys
+sys.path.append("..")
+from maximum_average_subarray_i import Solution
+
+class TestSolution(unittest.TestCase):
+
+    def test_case_1(self):
+        sol = Solution()
+        result = sol.findMaxAverage([1,12,-5,-6,50,3], 4)
+        self.assertEqual(result, 12.75000)
+
+    def test_case_2(self):
+        sol = Solution()
+        result = sol.findMaxAverage([5], 1)
+        self.assertEqual(result, 5.00000)
+
+if __name__ == '__main__':
+    unittest.main()
