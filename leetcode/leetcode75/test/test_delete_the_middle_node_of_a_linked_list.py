6a08710a422dfb33b835ba1d02de696cfdfd86dd
Tung Leo <tung.bquang@gmail.com>
2024-08-10 14:56:07 +0700

    LC75: Linked List part01 (#47)

---
+++ leetcode/leetcode75/test/test_delete_the_middle_node_of_a_linked_list.py
@@ -0,0 +1,11 @@
+import unittest
+import sys
+sys.path.append("..")
+from delete_the_middle_node_of_a_linked_list import Solution
+class TestSolution(unittest.TestCase):
+    def test_case_1(self):
+        sol = Solution()
+        # TODO
+
+if __name__ == '__main__':
+    unittest.main()
